1e95ed10abab301465512e09125e3cac62812e99
Timi-T <timiopebode@gmail.com>
2022-06-29 17:04:47 +0100

    filter by music style

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,14 @@
+-- Sql script to check lifespan of bands
+
+-- Select band name
+SELECT band_name AS "band_name",
+-- Calculate lifespan
+sum(split - formed) AS "lifespan"
+-- Specify table to select from
+FROM metal_bands
+-- Filter based on style
+WHERE style = "Glam rock"
+-- Group by the band name
+GROUP BY band_name
+-- Order by lifespan in descending order
+ORDER BY lifespan;
