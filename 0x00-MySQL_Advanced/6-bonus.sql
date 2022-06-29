776b53b6bd75d4afa25d233e1e358baba1e7ecd8
Timi-T <timiopebode@gmail.com>
2022-06-29 18:57:17 +0100

    created a stored procedure

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,10 @@
+DROP procedure IF EXISTS AddBonus;
+DELIMITER $$
+CREATE procedure AddBonus(IN user_id INT, IN project_name varchar(255), IN score INT)
+BEGIN
+    IF NOT EXISTS (SELECT name from projects WHERE name = project_name)
+        INSERT INTO projects (name) VALUES (project_name)
+    INSERT INTO corrections (user_id, project_id, score) values(user_id, (SELECT id FROM projects WHERE name = project_name), score);
+END;
+$$
+DELIMITER ;
