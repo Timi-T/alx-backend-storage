fe70a5d7c47e0224f7323c83f3f725f92abd0abf
Timi-T <timiopebode@gmail.com>
2022-06-29 21:01:19 +0100

    i sed an edif to terminate the if statement

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,12 +1,11 @@
 -- Sql script to create a procedure
 
-DROP procedure IF EXISTS AddBonus;
 DELIMITER $$
 CREATE procedure AddBonus(IN user_id INT, IN project_name varchar(255), IN score INT)
-BEGIN
-    IF NOT EXISTS (SELECT name from projects WHERE name = project_name)
-        INSERT INTO projects (name) VALUES (project_name)
-    INSERT INTO corrections (user_id, project_id, score) values(user_id, (SELECT id FROM projects WHERE name = project_name), score);
-END;
-$$
+    BEGIN
+    IF NOT EXISTS (SELECT name from projects WHERE projects.name = project_name)
+        THEN INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+END $$
 DELIMITER ;
