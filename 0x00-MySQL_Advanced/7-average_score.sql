3cad3ea629bd72026a8fa6a1af9f14ec0c9adf47
Timi-T <timiopebode@gmail.com>
2022-06-29 22:45:48 +0100

    select x from corrections c where c.user_id....

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,5 +2,5 @@
 
 CREATE procedure ComputeAverageScoreForUser(IN user_id INT)
 UPDATE users
-SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+SET average_score = (SELECT avg(score) FROM corrections c WHERE c.user_id = user_id)
 WHERE id = user_id;
