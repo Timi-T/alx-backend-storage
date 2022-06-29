8bab184d534da8f5b35f11429ea02e072632b89b
Timi-T <timiopebode@gmail.com>
2022-06-29 17:33:36 +0100

    make a column not nullable

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,8 +2,9 @@
 
 -- Create a table of users
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
     country ENUM("US", "CO", "TN") NOT NULL default(1)
+    PRIMARY KEY (id)
 );
