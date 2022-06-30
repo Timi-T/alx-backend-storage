afe6fc3b307045a9bb15594eb306973cc9d66629
Timi-T <timiopebode@gmail.com>
2022-06-30 17:21:23 +0100

    reset value to zero regardless of its previous value

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,11 +5,7 @@ CREATE TRIGGER update_email
 BEFORE UPDATE on users
 FOR EACH ROW
 IF (OLD.email != NEW.email)
-    THEN IF (NEW.valid_email = 1)
-        THEN SET NEW.valid_email = 0;
-    ELSE
-        SET NEW.valid_email = 1;
-    END IF;
+    THEN SET NEW.valid_email = 0;
 END IF;
 $$
 DELIMITER ;
