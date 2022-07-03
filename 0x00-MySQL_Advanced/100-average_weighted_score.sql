4f9745bbd5ba2c0abb8f213c164da738e83c2473
Timi-T <timiopebode@gmail.com>
2022-07-03 18:12:41 +0100

    join tables in a procedure

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,22 @@
+-- Sql script to create a stored procedure that calculates weighted scores
+
+DELIMITER $$
+CREATE procedure ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+UPDATE users
+SET average_score = FLOOR((SELECT SUM(
+    score / (
+        SELECT SUM(score) FROM (SELECT projects.name, projects.weight,
+        corrections.score FROM projects
+        JOIN corrections on projects.id = corrections.project_id
+        WHERE corrections.user_id = user_id) new
+    ) * score
+) 
+FROM (SELECT projects.name, projects.weight,
+corrections.score FROM projects
+JOIN corrections on projects.id = corrections.project_id
+WHERE corrections.user_id = user_id) a))
+WHERE id = user_id;
+END
+$$
+DELIMITER ;
