dbd3fe8b09d884e0767a84445b27211620070662
Timi-T <timiopebode@gmail.com>
2022-07-04 15:35:00 +0100

    insert into collection using python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module to add a new document to a mongodb database
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Return the new document id"""
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
