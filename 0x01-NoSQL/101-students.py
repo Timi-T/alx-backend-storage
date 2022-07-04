5e68331040a748943f3a747381e87e71c7845d15
Timi-T <timiopebode@gmail.com>
2022-07-04 21:42:28 +0100

    agrregation gallore

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/binenv python3
+"""
+Module to get average from scores
+"""
+
+
+def top_students(mongo_collection):
+    """Function to update documents with scores average"""
+    all_students = mongo_collection.aggregate(
+        [
+            {"$match": {}},
+            {"$unwind": "$topics"},
+            {"$group": {"_id": "$name", "averageScore": {"$avg": "$topics.score"}}},
+            {"$sort": {"averageScore": -1}},
+        ]
+    )
+    for student in all_students:
+        mongo_collection.update_one(
+            {"name": student.get("_id")},
+            {"$set": {"averageScore": student.get("averageScore")}}
+        )
+    all_students = mongo_collection.find().sort("averageScore", -1)
+    return all_students
